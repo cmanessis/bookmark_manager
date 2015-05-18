a00101f1c581048812d0eddf4491670d803c412b
cmanessis <cmpetrowii@gmail.com>
2015-05-18 17:44:55 +0100

    visit link url

---
+++ server.rb
@@ -0,0 +1,30 @@
+require 'data_mapper'
+require 'sinatra/base'
+
+class BookmarkManagerWeb < Sinatra::Base
+
+  get '/' do
+    @links = Link.all
+    erb :index
+  end
+
+  post '/links' do
+    url = params['url']
+    title = params['title']
+    Link.create(url: url, title: title)
+    redirect to('/')
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
+
+env = ENV['RACK_ENV'] || 'development'
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
