4ff206955ada5e061c238e87b4605c81888c7943
cmanessis <cmpetrowii@gmail.com>
2015-05-21 15:12:26 +0100

    updated data_mapper

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -8,6 +8,6 @@ require_relative 'models/user'
 # The name will be "bookmark_manager_test" or "bookmark_manager_development"
 # depending on the environment
 # DataMapper::Logger.new(STDOUT, :debug)
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['yljqbwqqtwptmn:4Sq4xxdBlwuS0LNIaboyCKZF2m@ec2-174-129-26-115.compute-1.amazonaws.com:5432/dcusv1j6fj1vsr'] || "postgres://localhost/bookmark_manager_#{env}")
 # After declaring your models, you should finalise them
 DataMapper.finalize
